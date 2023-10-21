80d2b8449139aa4203f365a23874f43e9b9a65c3
Harry Watkins <harry.watkins@bp.com>
2023-10-21 09:48:19 +0100

    Create board and empty grid in MainWindow

--- Logic/Board.cs
+++ Logic/Board.cs
@@ -2,5 +2,24 @@
 {
     public class Board
     {
+        int[,] board = new int[8, 8];
+
+        public Board()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                   if((i + j) % 2 == 0)
+                    {
+                        board[i, j] = 0;
+                    }
+                    else
+                    {
+                        board[i, j] = 1;
+                    }
+                }
+            }
+        }
     }
 }
