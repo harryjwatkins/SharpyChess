0284dc110070b0a3498bb5414ee80d6b06581a8f
Harry Watkins <harry.watkins@bp.com>
2023-11-12 13:01:27 +0000

    Add bishops to UI

---
+++ UI/Pieces/BlackBishop.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for BlackBishop.xaml
+    /// </summary>
+    public partial class BlackBishop : UserControl
+    {
+        public BlackBishop()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UI/Pieces/WhiteBishop.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for WhiteBishop.xaml
+    /// </summary>
+    public partial class WhiteBishop : UserControl
+    {
+        public WhiteBishop()
+        {
+            InitializeComponent();
+        }
+    }
+}
