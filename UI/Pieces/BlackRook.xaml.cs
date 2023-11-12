4a66b36476a2676f14d6655269d20fda096ac698
Harry Watkins <harry.watkins@bp.com>
2023-11-12 11:07:39 +0000

    Add rook icons

---
+++ UI/Pieces/BlackRook.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for BlackRook.xaml
+    /// </summary>
+    public partial class BlackRook : UserControl
+    {
+        public BlackRook()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UI/Pieces/WhiteRook.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for WhiteRook.xaml
+    /// </summary>
+    public partial class WhiteRook : UserControl
+    {
+        public WhiteRook()
+        {
+            InitializeComponent();
+        }
+    }
+}
