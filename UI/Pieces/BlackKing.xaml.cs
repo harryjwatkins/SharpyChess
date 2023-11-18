2fb50774d5ea3f9e61018a0d9609ba00e166f2c2
Harry Watkins <harry.watkins@bp.com>
2023-11-18 18:16:28 +0000

    Add kings to finish UI pieces

---
+++ UI/Pieces/BlackKing.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for BlackKing.xaml
+    /// </summary>
+    public partial class BlackKing : UserControl
+    {
+        public BlackKing()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UI/Pieces/WhiteKing.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for WhiteKing.xaml
+    /// </summary>
+    public partial class WhiteKing : UserControl
+    {
+        public WhiteKing()
+        {
+            InitializeComponent();
+        }
+    }
+}
