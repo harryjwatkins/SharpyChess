b6c6c124266b7b7c82d2b5fb8e2ad48181469c9b
Harry Watkins <harry.watkins@bp.com>
2023-11-18 18:08:55 +0000

    Add queen icons to UI

---
+++ UI/Pieces/BlackQueen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for BlackQueen.xaml
+    /// </summary>
+    public partial class BlackQueen : UserControl
+    {
+        public BlackQueen()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UI/Pieces/WhiteQueen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Pieces
+{
+    /// <summary>
+    /// Interaction logic for WhiteQueen.xaml
+    /// </summary>
+    public partial class WhiteQueen : UserControl
+    {
+        public WhiteQueen()
+        {
+            InitializeComponent();
+        }
+    }
+}
